e3228318cb74a11060f2545dd762993974e1be12
ou <1040393629qq.com>
2020-03-30 22:55:06 +0800

    搜索静态页面搭建完成

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,13 +1,26 @@
 <template>
-<div class="">
-    Search
-</div>
+  <div id="app">
+    <section class="search">
+      <header class="header">
+          <img class="search_sort" src="../../assets/images/order/up.png" alt="">
+      <form action="#" class="search_form">
+          <!-- <img class="search_icon" src="../../assets/images/order/search.png" alt=""> -->
+          <input 
+          type="search" 
+          name="search" 
+          placeholder="请输入商家或美食名称"
+          class="search_input">
+          <a class="search_text">搜索</a>
+      </form>
+      </header>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import './Search.styl'
 </style>
